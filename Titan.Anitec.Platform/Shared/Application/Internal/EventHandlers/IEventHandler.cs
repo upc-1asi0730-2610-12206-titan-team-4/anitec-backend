6d2a3deeb58a2efdc6fb2a7cce952f41d7bde823
Jorge Ayala <74685456+jorgeayaladev@users.noreply.github.com>
2026-06-05 17:59:21 -0500

    Delete Titan.Anitec.Platform directory

--- Titan.Anitec.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
+++
@@ -1,9 +0,0 @@
-using Anitec.Platform.Shared.Domain.Model.Events;
-using Cortex.Mediator.Notifications;
-
-
-namespace Anitec.Platform.Shared.Application.Internal.EventHandlers;
-
-public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
-{
-}
