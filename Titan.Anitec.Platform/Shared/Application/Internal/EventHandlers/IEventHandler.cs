01b0dc14f83fc33b41ac1259668754948bcc8bab
Melga1502 <melgarejojosep@gmail.com>
2026-06-05 17:38:45 -0500

    Update IEventHandler.cs

--- Titan.Anitec.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
+++ Titan.Anitec.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -1,8 +1,3 @@
 using Anitec.Platform.Shared.Domain.Model.Events;
 using Cortex.Mediator.Notifications;
 
-namespace Anitec.Platform.Shared.Application.Internal.EventHandlers;
-
-public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
-{
-}
