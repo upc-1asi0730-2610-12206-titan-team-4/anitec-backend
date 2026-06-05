cbe61844bb356cba2000d3eab4acb042779ad2cd
Melga1502 <melgarejojosep@gmail.com>
2026-06-05 17:37:28 -0500

    Refactor IEventHandler interface with proper namespaces

--- Titan.Anitec.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
+++ Titan.Anitec.Platform/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -1,3 +1,7 @@
+using Anitec.Platform.Shared.Domain.Model.Events;
+using Cortex.Mediator.Notifications;
+
+namespace Anitec.Platform.Shared.Application.Internal.EventHandlers;
 
 public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
 {
