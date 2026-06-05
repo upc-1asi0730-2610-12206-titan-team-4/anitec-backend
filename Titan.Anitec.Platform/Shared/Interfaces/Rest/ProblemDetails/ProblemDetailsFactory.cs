cb472a71dea6179e60be49f84a7b4bb6ed7ede9d
Abigail Raymundo Villarroel <abigail.raymundo.v@gmail.com>
2026-06-05 17:25:59 -0500

    Update ProblemDetailsFactory.cs

--- Titan.Anitec.Platform/Shared/Interfaces/Rest/ProblemDetails/ProblemDetailsFactory.cs
+++ Titan.Anitec.Platform/Shared/Interfaces/Rest/ProblemDetails/ProblemDetailsFactory.cs
@@ -33,7 +33,7 @@ public class ProblemDetailsFactory
         ControllerBase controller,
         int statusCode,
         Enum? errorEnum, // The specific error enum (IamError, ProfilesError, etc.)
-        string detailMessage) 
+        string detailMessage) // The localized message from the application service
     {
         // Leverage the base ProblemDetailsFactory for initial creation
         var problemDetails = _aspNetCoreProblemDetailsFactory.CreateProblemDetails( // Corrected usage
