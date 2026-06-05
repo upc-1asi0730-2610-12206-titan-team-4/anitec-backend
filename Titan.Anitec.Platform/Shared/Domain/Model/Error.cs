8e56fe3c7b86bc7f49c031db7d11f61f726ecf81
BrunoHG10 <u202117762@upc.edu.pe>
2026-06-05 17:38:18 -0500

    Update Error.cs

--- Titan.Anitec.Platform/Shared/Domain/Model/Error.cs
+++ Titan.Anitec.Platform/Shared/Domain/Model/Error.cs
@@ -5,4 +5,8 @@ namespace Anitec.Platform.Shared.Domain.Model;
 /// </summary>
 /// <param name="Code">The unique error code.</param>
 /// <param name="Message">The error message.</param>
-
+public record Error(string Code, string Message)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.");
+}
