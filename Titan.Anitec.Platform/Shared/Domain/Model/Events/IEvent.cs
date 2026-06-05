96c54e0034e78726b532c0768bd30d4c30e0aac5
BrunoHG10 <u202117762@upc.edu.pe>
2026-06-05 17:39:36 -0500

    Update IEvent.cs

--- Titan.Anitec.Platform/Shared/Domain/Model/Events/IEvent.cs
+++ Titan.Anitec.Platform/Shared/Domain/Model/Events/IEvent.cs
@@ -9,6 +9,3 @@ namespace Anitec.Platform.Shared.Domain.Model.Events;
 ///     This interface is used to mark classes as domain events that can be published and handled by the event bus.
 ///     It extends from <see cref="INotification" /> to integrate with the mediator pattern for event handling.
 /// </remarks>
-public interface IEvent : INotification
-{
-}
