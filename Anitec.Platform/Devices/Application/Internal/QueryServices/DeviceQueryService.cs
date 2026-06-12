a6e4300726325f01a3cfe3b5e70aa460472a5fed
Melga1502 <melgarejojosep@gmail.com>
2026-06-12 12:37:03 -0500

    feat; Refactor DeviceQueryService to implement IDeviceQueryService

--- Anitec.Platform/Devices/Application/Internal/QueryServices/DeviceQueryService.cs
+++ Anitec.Platform/Devices/Application/Internal/QueryServices/DeviceQueryService.cs
@@ -1,10 +1,23 @@
+using Anitec.Platform.Devices.Application.QueryServices;
 using Anitec.Platform.Devices.Domain.Model.Entities;
 using Anitec.Platform.Devices.Domain.Model.Queries;
+using Anitec.Platform.Devices.Domain.Repositories;
 
-namespace Anitec.Platform.Devices.Application.QueryServices;
+namespace Anitec.Platform.Devices.Application.Internal.QueryServices;
 
-public interface IDeviceQueryService
+public class DeviceQueryService(IDeviceRepository repository) : IDeviceQueryService
 {
-    Task<Device?> Handle(GetDeviceByIdQuery query, CancellationToken cancellationToken);
-    Task<IEnumerable<Device>> Handle(GetAllDevicesQuery query, CancellationToken cancellationToken);
+    public async Task<Device?> Handle(GetDeviceByIdQuery query, CancellationToken cancellationToken)
+    {
+        return await repository.FindByIdAsync(query.Id, cancellationToken);
+    }
+
+    public async Task<IEnumerable<Device>> Handle(GetAllDevicesQuery query, CancellationToken cancellationToken)
+    {
+        return await repository.ListAsync(cancellationToken);
+    }
 }
+
+
+
+
