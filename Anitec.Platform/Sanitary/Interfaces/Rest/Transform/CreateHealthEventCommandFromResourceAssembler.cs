04fa5a603a7c5cd8cdba1ee8435a1bd9a086eff6
LS <143048016+luccsss@users.noreply.github.com>
2026-06-12 11:42:55 -0500

    Remove CreateHealthEventCommandFromResourceAssembler class

--- Anitec.Platform/Sanitary/Interfaces/Rest/Transform/CreateHealthEventCommandFromResourceAssembler.cs
+++ Anitec.Platform/Sanitary/Interfaces/Rest/Transform/CreateHealthEventCommandFromResourceAssembler.cs
@@ -1,12 +1,1 @@
-using Anitec.Platform.Sanitary.Domain.Model.Commands;
-using Anitec.Platform.Sanitary.Interfaces.Rest.Resources;
 
-namespace Anitec.Platform.Sanitary.Interfaces.Rest.Transform;
-
-public static class CreateHealthEventCommandFromResourceAssembler
-{
-    public static CreateHealthEventCommand ToCommandFromResource(CreateHealthEventResource resource)
-    {
-        return new CreateHealthEventCommand(resource.AnimalId, resource.Type, resource.Date, resource.Description, resource.Veterinarian, resource.Diagnosis, resource.Treatment, resource.Prescription, resource.FollowUp, resource.NextDueDate);
-    }
-}
