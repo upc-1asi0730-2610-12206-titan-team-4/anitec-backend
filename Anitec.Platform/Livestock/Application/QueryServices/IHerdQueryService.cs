5aece365c2dc34431f68cc18567b7098a45f56db
BrunoHG10 <u202117762@upc.edu.pe>
2026-06-12 12:36:16 -0500

    Update IHerdQueryService.cs

---
+++ Anitec.Platform/Livestock/Application/QueryServices/IHerdQueryService.cs
@@ -0,0 +1,14 @@
+using Anitec.Platform.Livestock.Domain.Model.Entities;
+using Anitec.Platform.Livestock.Domain.Model.Queries;
+
+namespace Anitec.Platform.Livestock.Application.QueryServices;
+
+public interface IHerdQueryService
+{
+    Task<Herd?> Handle(GetHerdByIdQuery query, CancellationToken cancellationToken);
+    Task<IEnumerable<Herd>> Handle(GetAllHerdsQuery query, CancellationToken cancellationToken);
+}
+
+
+
+
