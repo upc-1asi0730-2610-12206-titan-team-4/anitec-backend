5447502dbf8964e2517085c3402b550ed397aacb
Melga1502 <melgarejojosep@gmail.com>
2026-06-12 11:19:01 -0500

    feat: add IDeviceRepository interface for device management

---
+++ Anitec.Platform/Devices/Domain/Repositories/IDeviceRepository.cs
@@ -0,0 +1,8 @@
+using Anitec.Platform.Devices.Domain.Model.Entities;
+using Anitec.Platform.Shared.Domain.Repositories;
+
+namespace Anitec.Platform.Devices.Domain.Repositories;
+
+public interface IDeviceRepository : IBaseRepository<Device>
+{
+}
