df1c4bd7e47f56b325ab7bff23b068e57cc4ccf4
Melga1502 <melgarejojosep@gmail.com>
2026-06-12 11:15:38 -0500

    feat: add IDeviceQueryService interface for device queries

---
+++ Anitec.Platform/Devices/Application/QueryServices/IDeviceQueryService.cs
@@ -0,0 +1,14 @@
+using Anitec.Platform.Devices.Domain.Model.Entities;
+using Anitec.Platform.Devices.Domain.Model.Queries;
+
+namespace Anitec.Platform.Devices.Application.QueryServices;
+
+public interface IDeviceQueryService
+{
+    Task<Device?> Handle(GetDeviceByIdQuery query, CancellationToken cancellationToken);
+    Task<IEnumerable<Device>> Handle(GetAllDevicesQuery query, CancellationToken cancellationToken);
+}
+
+
+
+
