f6bb88662eb33e299ee3061778ca07c9e330b22e
Abigail Raymundo Villarroel <abigail.raymundo.v@gmail.com>
2026-06-12 12:13:19 -0500

    feat: Add IReportMetricRepository interface

---
+++ Anitec.Platform/Analytics/Domain/Repositories/IReportMetricRepository.cs
@@ -0,0 +1,8 @@
+using Anitec.Platform.Analytics.Domain.Model.Entities;
+using Anitec.Platform.Shared.Domain.Repositories;
+
+namespace Anitec.Platform.Analytics.Domain.Repositories;
+
+public interface IReportMetricRepository : IBaseRepository<ReportMetric>
+{
+}
