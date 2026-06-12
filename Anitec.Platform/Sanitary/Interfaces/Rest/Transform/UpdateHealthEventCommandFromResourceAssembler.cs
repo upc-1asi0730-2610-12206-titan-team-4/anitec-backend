ff8fe1ff692c43a287db7cdcdf0ea3dfcd3c558a
LS <143048016+luccsss@users.noreply.github.com>
2026-06-12 18:11:25 -0500

    Add UpdateHealthEventCommandFromResourceAssembler

--- Anitec.Platform/Sanitary/Interfaces/Rest/Transform/UpdateHealthEventCommandFromResourceAssembler.cs
+++ Anitec.Platform/Sanitary/Interfaces/Rest/Transform/UpdateHealthEventCommandFromResourceAssembler.cs
@@ -1,1 +1,12 @@
+using Anitec.Platform.Sanitary.Domain.Model.Commands;
+using Anitec.Platform.Sanitary.Interfaces.Rest.Resources;
 
+namespace Anitec.Platform.Sanitary.Interfaces.Rest.Transform;
+
+public static class UpdateHealthEventCommandFromResourceAssembler
+{
+    public static UpdateHealthEventCommand ToCommandFromResource(int id, CreateHealthEventResource resource)
+    {
+        return new UpdateHealthEventCommand(id, resource.AnimalId, resource.Type, resource.Date, resource.Description, resource.Veterinarian, resource.Diagnosis, resource.Treatment, resource.Prescription, resource.FollowUp, resource.NextDueDate);
+    }
+}
