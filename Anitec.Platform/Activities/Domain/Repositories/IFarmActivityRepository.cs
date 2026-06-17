00e1be0077f88e5374c3d01813cefd5ccf843114
Melga1502 <melgarejojosep@gmail.com>
2026-06-17 01:11:40 -0500

    feat: add IFarmActivityRepository interface

---
+++ Anitec.Platform/Activities/Domain/Repositories/IFarmActivityRepository.cs
@@ -0,0 +1,8 @@
+using Anitec.Platform.Activities.Domain.Model.Entities;
+using Anitec.Platform.Shared.Domain.Repositories;
+
+namespace Anitec.Platform.Activities.Domain.Repositories;
+
+public interface IFarmActivityRepository : IBaseRepository<FarmActivity>
+{
+}
