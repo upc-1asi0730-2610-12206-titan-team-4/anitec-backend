ffd6cdb7b2af6f2a1a7f230f55ff6ca1afb614db
Jorge Ayala <74685456+jorgeayaladev@users.noreply.github.com>
2026-06-16 23:15:53 -0500

    feat: add IDeviceMetricRepository interface

---
+++ Anitec.Platform/Metrics/Infrastructure/Persistence/EntityFrameworkCore/Repositories/DeviceMetricRepository.cs
@@ -0,0 +1,8 @@
+using Anitec.Platform.Metrics.Domain.Model.Entities;
+using Anitec.Platform.Shared.Domain.Repositories;
+
+namespace Anitec.Platform.Metrics.Domain.Repositories;
+
+public interface IDeviceMetricRepository : IBaseRepository<DeviceMetric>
+{
+}
