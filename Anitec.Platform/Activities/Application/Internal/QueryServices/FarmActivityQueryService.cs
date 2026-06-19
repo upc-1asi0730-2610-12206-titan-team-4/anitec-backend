fd6a29b61e40bf2dc08bbc8b4f3d69a754caa5dd
Melga1502 <melgarejojosep@gmail.com>
2026-06-19 00:52:31 -0500

    feat: add FarmActivityQueryService implementation
    Removed implementation of FarmActivityQueryService class.

--- Anitec.Platform/Activities/Application/Internal/QueryServices/FarmActivityQueryService.cs
+++ Anitec.Platform/Activities/Application/Internal/QueryServices/FarmActivityQueryService.cs
@@ -5,18 +5,6 @@ using Anitec.Platform.Activities.Domain.Repositories;
 
 namespace Anitec.Platform.Activities.Application.Internal.QueryServices;
 
-public class FarmActivityQueryService(IFarmActivityRepository repository) : IFarmActivityQueryService
-{
-    public async Task<FarmActivity?> Handle(GetFarmActivityByIdQuery query, CancellationToken cancellationToken)
-    {
-        return await repository.FindByIdAsync(query.Id, cancellationToken);
-    }
-
-    public async Task<IEnumerable<FarmActivity>> Handle(GetAllFarmActivitiesQuery query, CancellationToken cancellationToken)
-    {
-        return await repository.ListAsync(cancellationToken);
-    }
-}
 
 
 
