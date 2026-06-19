eb8c8c7b749390fd38ddd2314077ea4ef9776e4d
BrunoHG10 <u202117762@upc.edu.pe>
2026-06-19 01:05:21 -0500

    Update ISubscriptionRepository.cs

--- Anitec.Platform/Subscriptions/Domain/Repositories/ISubscriptionRepository.cs
+++ Anitec.Platform/Subscriptions/Domain/Repositories/ISubscriptionRepository.cs
@@ -1,1 +1,9 @@
+using Anitec.Platform.Subscriptions.Domain.Model.Entities;
+using Anitec.Platform.Shared.Domain.Repositories;
+
+namespace Anitec.Platform.Subscriptions.Domain.Repositories;
+
+public interface ISubscriptionRepository : IBaseRepository<Subscription>
+{
+}
 
