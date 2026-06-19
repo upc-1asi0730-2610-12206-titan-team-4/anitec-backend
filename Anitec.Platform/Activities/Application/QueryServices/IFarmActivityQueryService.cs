a4995b7658da4fe2f87faddf2136bf26effa7933
Melga1502 <melgarejojosep@gmail.com>
2026-06-18 23:45:17 -0500

    feat: change interface methods from IFarmActivityQueryService

--- Anitec.Platform/Activities/Application/QueryServices/IFarmActivityQueryService.cs
+++ Anitec.Platform/Activities/Application/QueryServices/IFarmActivityQueryService.cs
@@ -1,14 +1,6 @@
 using Anitec.Platform.Activities.Domain.Model.Entities;
 using Anitec.Platform.Activities.Domain.Model.Queries;
 
-namespace Anitec.Platform.Activities.Application.QueryServices;
-
-public interface IFarmActivityQueryService
-{
-    Task<FarmActivity?> Handle(GetFarmActivityByIdQuery query, CancellationToken cancellationToken);
-    Task<IEnumerable<FarmActivity>> Handle(GetAllFarmActivitiesQuery query, CancellationToken cancellationToken);
-}
-
 
 
 
