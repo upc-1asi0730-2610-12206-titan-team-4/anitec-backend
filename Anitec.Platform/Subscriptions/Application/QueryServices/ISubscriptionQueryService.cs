3be207bc1d46b8252d6bd8c04bb70da596dd2aee
BrunoHG10 <u202117762@upc.edu.pe>
2026-06-19 01:03:13 -0500

    Update ISubscriptionQueryService.cs

--- Anitec.Platform/Subscriptions/Application/QueryServices/ISubscriptionQueryService.cs
+++ Anitec.Platform/Subscriptions/Application/QueryServices/ISubscriptionQueryService.cs
@@ -1,1 +1,15 @@
+using Anitec.Platform.Subscriptions.Domain.Model.Entities;
+using Anitec.Platform.Subscriptions.Domain.Model.Queries;
+
+namespace Anitec.Platform.Subscriptions.Application.QueryServices;
+
+public interface ISubscriptionQueryService
+{
+    Task<Subscription?> Handle(GetSubscriptionByIdQuery query, CancellationToken cancellationToken);
+    Task<IEnumerable<Subscription>> Handle(GetAllSubscriptionsQuery query, CancellationToken cancellationToken);
+}
+
+
+
+
 
