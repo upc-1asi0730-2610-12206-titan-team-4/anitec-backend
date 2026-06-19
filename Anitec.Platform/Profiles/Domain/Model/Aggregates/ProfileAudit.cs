b2df24e7114d18a6e69224b6579fc1a6b87a42f7
Jorge Ayala <74685456+jorgeayaladev@users.noreply.github.com>
2026-06-18 23:40:15 -0500

    feat: init profiles folder structure with all jerarchy

---
+++ Anitec.Platform/Profiles/Domain/Model/Aggregates/ProfileAudit.cs
@@ -0,0 +1,9 @@
+using Anitec.Platform.Shared.Domain.Model.Entities;
+
+namespace Anitec.Platform.Profiles.Domain.Model.Aggregates;
+
+public partial class Profile : IAuditableEntity
+{
+    public DateTimeOffset? CreatedAt { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
---
+++ Anitec.Platform/Profiles/Domain/Model/Errors/ProfileErrors.cs
@@ -0,0 +1,12 @@
+using Anitec.Platform.Shared.Domain.Model;
+
+namespace Anitec.Platform.Profiles.Domain.Model.Errors;
+
+public static class ProfileErrors
+{
+    public static readonly Error ProfileCreationFailed =
+        new("Profiles.ProfileCreationFailed", "An error occurred while creating the profile.");
+
+    public static readonly Error ProfileNotFound =
+        new("Profiles.ProfileNotFound", "The specified profile was not found.");
+}
