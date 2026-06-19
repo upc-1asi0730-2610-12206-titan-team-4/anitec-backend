53f24b044c73b797c00b0e599d4c9488a994836d
Melga1502 <melgarejojosep@gmail.com>
2026-06-18 23:44:06 -0500

    feat: add IFarmActivityCommandService interface

--- Anitec.Platform/Activities/Application/CommandServices/IFarmActivityCommandService.cs
+++ Anitec.Platform/Activities/Application/CommandServices/IFarmActivityCommandService.cs
@@ -3,3 +3,11 @@ using Anitec.Platform.Activities.Domain.Model;
 using Anitec.Platform.Activities.Domain.Model.Entities;
 using Anitec.Platform.Shared.Application.Model;
 
+namespace Anitec.Platform.Activities.Application.CommandServices;
+
+public interface IFarmActivityCommandService
+{
+    Task<Result<FarmActivity>> Handle(CreateFarmActivityCommand command, CancellationToken cancellationToken);
+    Task<Result<FarmActivity>> Handle(UpdateFarmActivityCommand command, CancellationToken cancellationToken);
+    Task<Result> Handle(DeleteFarmActivityCommand command, CancellationToken cancellationToken);
+}
