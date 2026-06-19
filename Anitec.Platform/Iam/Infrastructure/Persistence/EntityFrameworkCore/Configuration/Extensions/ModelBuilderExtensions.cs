d5cd045669a2a220d498ffabfa03e9590de573d2
LS <143048016+luccsss@users.noreply.github.com>
2026-06-19 00:54:22 -0500

    Reformat ModelBuilderExtensions.cs with consistent spacing

--- Anitec.Platform/Iam/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
+++ Anitec.Platform/Iam/Infrastructure/Persistence/EntityFrameworkCore/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -8,7 +8,6 @@ public static class ModelBuilderExtensions
     public static void ApplyIamConfiguration(this ModelBuilder builder)
     {
         // IAM Context
-
         builder.Entity<User>().HasKey(u => u.Id);
         builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
         builder.Entity<User>().Property(u => u.Username).IsRequired().HasMaxLength(80);
