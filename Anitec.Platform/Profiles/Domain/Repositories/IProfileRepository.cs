954723c9d87b6ed8e9a70e55cf549d9877a02c77
Jorge Ayala <74685456+jorgeayaladev@users.noreply.github.com>
2026-06-19 01:01:43 -0500

    Remove XML comments from IProfileRepository
    Removed XML documentation for FindProfileByEmailAsync method.

--- Anitec.Platform/Profiles/Domain/Repositories/IProfileRepository.cs
+++ Anitec.Platform/Profiles/Domain/Repositories/IProfileRepository.cs
@@ -9,15 +9,6 @@ namespace Anitec.Platform.Profiles.Domain.Repositories;
 /// </summary>
 public interface IProfileRepository : IBaseRepository<Profile>
 {
-    /// <summary>
-    ///     Find a profile by email
-    /// </summary>
-    /// <param name="email">
-    ///     The <see cref="EmailAddress" /> email address to search for
-    /// </param>
-    /// <param name="cancellationToken">The cancellation token.</param>
-    /// <returns>
-    ///     The <see cref="Profile" /> if found, otherwise null
-    /// </returns>
+    
     Task<Profile?> FindProfileByEmailAsync(EmailAddress email, CancellationToken cancellationToken);
 }
