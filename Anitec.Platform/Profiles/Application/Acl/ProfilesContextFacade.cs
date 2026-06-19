91e7ff83e5b9414c45341e91b06dae513c24d609
Jorge Ayala <74685456+jorgeayaladev@users.noreply.github.com>
2026-06-18 23:59:57 -0500

    feat: remove FetchProfileIdByEmail method
    Removed the FetchProfileIdByEmail method from ProfilesContextFacade.

--- Anitec.Platform/Profiles/Application/Acl/ProfilesContextFacade.cs
+++ Anitec.Platform/Profiles/Application/Acl/ProfilesContextFacade.cs
@@ -32,11 +32,5 @@ public class ProfilesContextFacade(
         return result.Value?.Id ?? 0;
     }
 
-    // inheritedDoc
-    public async Task<int> FetchProfileIdByEmail(string email, CancellationToken cancellationToken)
-    {
-        var getProfileByEmailQuery = new GetProfileByEmailQuery(new EmailAddress(email));
-        var profile = await profileQueryService.Handle(getProfileByEmailQuery, cancellationToken);
-        return profile?.Id ?? 0;
-    }
+    
 }
