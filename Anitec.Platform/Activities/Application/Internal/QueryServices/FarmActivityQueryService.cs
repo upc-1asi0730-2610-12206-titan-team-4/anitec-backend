0860338697aaa88dc748a69c3d2f78f0e434c640
Melga1502 <melgarejojosep@gmail.com>
2026-06-19 00:53:26 -0500

    feat: Remove unnecessary using directives in FarmActivityQueryService

--- Anitec.Platform/Activities/Application/Internal/QueryServices/FarmActivityQueryService.cs
+++ Anitec.Platform/Activities/Application/Internal/QueryServices/FarmActivityQueryService.cs
@@ -1,7 +1,4 @@
-using Anitec.Platform.Activities.Application.QueryServices;
-using Anitec.Platform.Activities.Domain.Model.Entities;
-using Anitec.Platform.Activities.Domain.Model.Queries;
-using Anitec.Platform.Activities.Domain.Repositories;
+
 
 namespace Anitec.Platform.Activities.Application.Internal.QueryServices;
 
