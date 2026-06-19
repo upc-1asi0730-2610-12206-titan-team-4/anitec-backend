03e9882a33ba5d7afa5263054ca0bed50748b7ce
BrunoHG10 <u202117762@upc.edu.pe>
2026-06-19 00:02:49 -0500

    Update SubscriptionRepository.cs

---
+++ Anitec.Platform/Subscriptions/Infrastructure/Persistence/EntityFrameworkCore/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,10 @@
+using Anitec.Platform.Subscriptions.Domain.Model.Entities;
+using Anitec.Platform.Subscriptions.Domain.Repositories;
+using Anitec.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Configuration;
+using Anitec.Platform.Shared.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+namespace Anitec.Platform.Subscriptions.Infrastructure.Persistence.EntityFrameworkCore.Repositories;
+
+public class SubscriptionRepository(AppDbContext context) : BaseRepository<Subscription>(context), ISubscriptionRepository
+{
+}
