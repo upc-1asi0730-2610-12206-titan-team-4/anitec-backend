d085fedfe0a42a6ca268fd70464dace8dd37fdff
Melga1502 <melgarejojosep@gmail.com>
2026-06-19 00:53:38 -0500

    feat: add FarmActivityQueryService with added usings

--- Anitec.Platform/Activities/Application/Internal/QueryServices/FarmActivityQueryService.cs
+++ Anitec.Platform/Activities/Application/Internal/QueryServices/FarmActivityQueryService.cs
@@ -1,4 +1,7 @@
-
+using Anitec.Platform.Activities.Application.QueryServices;
+using Anitec.Platform.Activities.Domain.Model.Entities;
+using Anitec.Platform.Activities.Domain.Model.Queries;
+using Anitec.Platform.Activities.Domain.Repositories;
 
 namespace Anitec.Platform.Activities.Application.Internal.QueryServices;
 
