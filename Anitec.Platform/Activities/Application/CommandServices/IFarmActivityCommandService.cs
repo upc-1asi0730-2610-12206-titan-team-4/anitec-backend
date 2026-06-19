e3469e4baa8c836a1f7162ba281b68ebfd73bad7
Melga1502 <melgarejojosep@gmail.com>
2026-06-18 23:44:32 -0500

    feat: add IFarmActivityCommandService references

--- Anitec.Platform/Activities/Application/CommandServices/IFarmActivityCommandService.cs
+++ Anitec.Platform/Activities/Application/CommandServices/IFarmActivityCommandService.cs
@@ -1,7 +1,3 @@
-using Anitec.Platform.Activities.Domain.Model.Commands;
-using Anitec.Platform.Activities.Domain.Model;
-using Anitec.Platform.Activities.Domain.Model.Entities;
-using Anitec.Platform.Shared.Application.Model;
 
 namespace Anitec.Platform.Activities.Application.CommandServices;
 
