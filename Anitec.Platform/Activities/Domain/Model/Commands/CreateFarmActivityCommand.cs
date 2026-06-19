119d11b59c7ce44555930b718f20036507c111ab
Melga1502 <melgarejojosep@gmail.com>
2026-06-19 00:08:35 -0500

    feat: Add CreateFarmActivityCommand record definition

--- Anitec.Platform/Activities/Domain/Model/Commands/CreateFarmActivityCommand.cs
+++ Anitec.Platform/Activities/Domain/Model/Commands/CreateFarmActivityCommand.cs
@@ -1,2 +1,3 @@
 namespace Anitec.Platform.Activities.Domain.Model.Commands;
 
+public record CreateFarmActivityCommand(int? OwnerId, int? VeterinarianId, string Title, string Type, DateOnly Date, string Priority, string Status);
